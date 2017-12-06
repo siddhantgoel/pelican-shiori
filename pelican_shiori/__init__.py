b2603a86a4e4c140daeb674faf3c281178efc17a
Siddhant Goel <siddhantgoel@gmail.com>
2017-12-06 18:55:39 +0100

    asset_config function to return webassets config

--- pelican_shiori/__init__.py
+++ pelican_shiori/__init__.py
@@ -1,6 +1,13 @@
 from pathlib import Path
 
 
-# https://github.com/getpelican/pelican/issues/1564#issuecomment-282136049
-def get_path():
+def asset_config(theme):
+    return (
+        ('JINJA2_CONTEXT', {'theme': theme}),
+    )
+
+
+def path():
+    """https://github.com/getpelican/pelican/issues/1564#issuecomment-282136049
+    """
     return str(Path(__file__).parent)
