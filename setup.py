2a4381b836785aaaf38bc238739df24b9964a948
Siddhant Goel <siddhantgoel@gmail.com>
2017-12-07 11:55:48 +0100

    update setup.py args

--- setup.py
+++ setup.py
@@ -9,11 +9,17 @@ setup(
         'Development Status :: 4 - Beta',
         'Framework :: Pelican :: Themes',
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
     include_package_data=True,
     install_requires=['jinja2', 'webassets'],
     packages=['pelican_shiori'],
     url='https://github.com/siddhantgoel/pelican-shiori',
     version='0.1.0',
+    license='MIT',
+    keywords='python pelican pelican-theme static-site'
 )
