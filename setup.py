4004fb242b34456798f676cf6a81618e2f0786ab
Siddhant Goel <siddhantgoel@gmail.com>
2017-12-08 09:10:24 +0100

    bump version

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(
     install_requires=['cssmin', 'jinja2', 'webassets'],
     packages=['pelican_shiori'],
     url='https://github.com/siddhantgoel/pelican-shiori',
-    version='0.1.1',
+    version='0.2.0',
     license='MIT',
     keywords='python pelican pelican-theme static-site'
 )
